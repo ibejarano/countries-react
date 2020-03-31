c487bc9ef406a871595e5f7a6cec1c706deb3f7d
Ignacio Bejarano <ignbejarano@gmail.com>
2020-03-31 15:28:36 -0300

    added filter fields

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect}  from 'react';
+import axios from 'axios';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import HomeCountries from './pages/HomeCountries'
 import CountryPage from './pages/CountryPage'
 
 export default function App(){
+  const [countries, setCountries] = useState([]);
 
+  useEffect(() => {
+    axios.get('https://restcountries.eu/rest/v2/all').then(({data}) => setCountries(data)  ).catch(console.log)
+}, [])
+
+console.log(countries)
   return(
     <div>
       <h1>My country app</h1>
       <Router>
           <Switch>
               <Route exact  path="/" >
-                  <HomeCountries />
+                  <HomeCountries countries={countries} />
               </Route>
               <Route exact path="/:countryCode" children={<CountryPage />}  />
           </Switch>
--- src/pages/HomeCountries.js
+++ src/pages/HomeCountries.js
@@ -1,20 +1,22 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React, {useState} from 'react';
 import CountryCard from '../components/CountryCard'
 
-export default function HomeCountries(){
-  const [countries, setCountries] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-
-  useEffect(() => {
-      axios.get('https://restcountries.eu/rest/v2/all').then(({data}) => setCountries(data)  ).catch(console.log)
-  }, [])
-  console.log(countries)
+export default function HomeCountries({countries}){
+  const [filter , setFilter ] = useState(null)
 
   return(
-    <div className='country-cards-container'>
-      {countries.map(country => <CountryCard  key={country.name}  {...country}  />  )}
+    <div >
+      <div>
+      <button onClick={() => setFilter(null)}>Remove</button>
+        <button onClick={() => setFilter('Africa')}>Africa</button>
+        <button onClick={() => setFilter('Americas')}>Americas</button>
+        <button onClick={() => setFilter('Asia')}>Asia</button>
+        <button onClick={() => setFilter('Europe')}>Europe</button>
+        <button onClick={() => setFilter('Oceania')}>Oceania</button>
+      </div>
+      <div className='country-cards-container'>
+      {countries.filter(country => !filter || country.region===filter  ).map(country => <CountryCard  key={country.name}  {...country}  />  )}
+      </div>
     </div>
   )
 }
