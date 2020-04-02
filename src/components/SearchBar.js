13f26e2a49ae2e5e3ac1f7a4efe36bcfb10d2298
Ignacio Bejarano <ignbejarano@gmail.com>
2020-04-02 20:52:51 -0300

    [ADD] Styled search bar styled component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,7 @@
-import React, {useState} from 'react';
-import {Link} from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import StyledSearchBar from "./styled/StyledSearchBar";
+import SearchIcon from "@material-ui/icons/Search"
 
 // const countryMocks = [{name: 'Argentina', aplha3Code:'ARG'},
 // {name: 'Brasil'},
@@ -10,30 +12,39 @@ import {Link} from 'react-router-dom'
 // {name: 'Alemania'},
 // {name: 'Holanda'},
 // ]
-let resultLink = []
+let resultLink = [];
 
-const SearchBar = ({countries}) => {
-    const [searchText, setSearchText] = useState('')
-    const handleChange = (e) => {
-        setSearchText(e.target.value)
-    }
+const SearchBar = ({ countries }) => {
+  const [searchText, setSearchText] = useState("");
+  const handleChange = e => {
+    setSearchText(e.target.value);
+  };
 
-    resultLink = countries.filter(country => country.name.toLowerCase().includes(searchText.toLowerCase()))
+  resultLink = countries.filter(country =>
+    country.name.toLowerCase().includes(searchText.toLowerCase())
+  );
 
-    return (
-        <div>
-            <input type='text'
-            onChange={handleChange}
-            placeholder="Search" 
-            value={searchText}
-            />
-            <div>
-    { searchText !== '' && 
-    resultLink.map(res => <Link to={res.alpha3Code.toLowerCase()} key={res.name}>{res.name}</Link>)
-    }
-            </div>
-        </div>
-    );
+  return (
+    <StyledSearchBar>
+      <span>
+        <SearchIcon />
+      </span>
+      <input
+        type="text"
+        onChange={handleChange}
+        placeholder="Search for a country..."
+        value={searchText}
+      />
+      <div>
+        {searchText !== "" &&
+          resultLink.map(res => (
+            <Link to={res.alpha3Code.toLowerCase()} key={res.name}>
+              {res.name}
+            </Link>
+          ))}
+      </div>
+    </StyledSearchBar>
+  );
 };
 
 export default SearchBar;
---
+++ src/components/styled/StyledSearchBar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledSearchBar = styled.div`
+  display: flex;
+  flex-direction: row nowrap;
+  height: 30px;
+  padding: 7px;
+  box-shadow: 0px 0px 4px 4px rgba(0,0,0,0.05);
+  input {
+    width: 80%;
+    border: 0 white solid;
+    font-size: 14px;
+    flex-grow:5;
+  }
+  span {
+      flex-grow: 1;
+      margin: auto 1rem;
+      padding: 0;
+  }
+  margin: 26px;
+`;
+
+export default StyledSearchBar;
