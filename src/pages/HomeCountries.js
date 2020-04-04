b35338826f6ba0327abf9ec184b97517bfd83f08
Ignacio Bejarano <ignbejarano@gmail.com>
2020-04-04 09:59:07 -0300

    [ADD] main page desktop style

--- src/pages/HomeCountries.js
+++ src/pages/HomeCountries.js
@@ -2,6 +2,11 @@ import React, {useState} from 'react';
 import CountryCard from '../components/CountryCard'
 import FilterDropdown from '../components/FilterDropdown'
 
+const CountryContainerStyle = {
+  display:'flex',
+  flexFlow: 'row wrap'
+}
+
 export default function HomeCountries({countries}){
   const [activeFilter , setActiveFilter ] = useState('')
   return(
@@ -10,7 +15,7 @@ export default function HomeCountries({countries}){
       setActiveFilter={setActiveFilter} 
       activeFilter={activeFilter}
       />
-      <div className='country-cards-container'>
+      <div className='country-cards-container' style={CountryContainerStyle} >
       {countries.filter(country => !activeFilter || country.region===activeFilter  ).map(country => <CountryCard  key={country.name}  {...country}  />  )}
       </div>
     </div>
