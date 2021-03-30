6fbb69feab65df2ead76378081e67dbc45c2c1e8
Ignacio Bejarano <ignbejarano@gmail.com>
2021-03-30 18:05:13 -0300

    removed deprecated files

--- src/components/Container.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-const Container = styled.div`
-  background: ${(props) => props.theme.color.mainBackground};
-  padding: 50px;
-`;
-
-export default Container;
